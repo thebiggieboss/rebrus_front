661201164c8fc17ceda5316985bc07e3b170e67d
headhoncho <thebigboss@internet.ru>
2024-05-26 05:37:59 +0500

    веч коммит files

--- src/app/modules/files/files.component.ts
+++ src/app/modules/files/files.component.ts
@@ -1,12 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { NzUploadFile } from 'ng-zorro-antd/upload';
 
 @Component({
   selector: 'app-files',
   templateUrl: './files.component.html',
   styleUrls: ['./files.component.scss'],
 })
-export class FilesComponent implements OnInit {
+export class FilesComponent implements OnInit, OnDestroy {
+  public s: Subscription[] = [];
+  public tableData = [
+    {
+      fileName: 'ТЕСТ_МОНРЕАЛ_2019.docx',
+      description: 'Тесты архив',
+      creationDate: '20.07.2024',
+    },
+    {
+      fileName: 'ТЕСТ_МОНРЕАЛ_2019.docx',
+      description: 'Тесты архив',
+      creationDate: '19.07.2024',
+    },
+  ];
+  public isVisibleModal: boolean = false;
+  fileList: NzUploadFile[] = [];
+  previewImage: string | undefined = '';
+  previewVisible = false;
+  public fileName: string = '';
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngOnDestroy() {
+    this.s.forEach(s => s.unsubscribe());
+  }
+
+  actions(type: string) {}
+
+  onOpenModal() {
+    this.isVisibleModal = !this.isVisibleModal;
+  }
+
+  handleCancel() {
+    this.isVisibleModal = !this.isVisibleModal;
+  }
+
+  handleOk() {
+    console.log(this.fileList);
+  }
+
+  handlePreview = async (file: NzUploadFile) => {
+    this.previewImage = file.url || file.thumbUrl;
+    this.previewVisible = true;
+  };
+
+  beforeUpload = (file: NzUploadFile, fileList: NzUploadFile[]) => {
+    return () => {
+      return new Promise<boolean>((resolve, reject) => {
+        return false;
+      });
+    };
+  };
 }
--- src/app/modules/files/files.module.ts
+++ src/app/modules/files/files.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { FilesComponent } from './files.component';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../../shared';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
 
 const routes: Routes = [
   {
@@ -12,7 +15,14 @@ const routes: Routes = [
 ];
 @NgModule({
   declarations: [FilesComponent],
-  imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild(routes),
+    NzDropDownModule,
+    NzModalModule,
+    NzUploadModule,
+  ],
   exports: [RouterModule],
 })
 export class FilesModule {}
